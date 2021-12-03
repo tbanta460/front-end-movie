35a506bab10c59310b7001bbd27b78af41a49f17
tbanta460 <chitandasu@yahoo.com>
2021-12-03 07:00:22 +0700

    Menghapus folder component root folder

--- component/store/rootReducer.jsx
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-const { myReducers } = require("./reducers/reducers");
-
-export const rootReducer = combineReducers({
-    myState: myReducers
-})
--- component/store/store.jsx
+++
@@ -1,5 +0,0 @@
-import {createStore} from 'redux'
-import { myReducers } from "./reducers/reducers";
-import { rootReducer } from './rootReducer'
-// console.log(rootReducer)
-export const store = createStore(rootReducer)
